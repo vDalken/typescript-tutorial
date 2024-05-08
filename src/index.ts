acd28a862ecbeddebfe145787b381bd6d9751cf8
Fábio <fabiogoncalvesk2001@gmail.com>
2024-05-08 19:47:23 +0100

    add typeorm, make connection to database and  create table book

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-import express, {Express} from 'express'
+import 'reflect-metadata'
+import express, { Express } from 'express'
 import routes from './routes/index'
 import dotenv from 'dotenv'
+import { DataSource } from 'typeorm'
+import { Book } from './models/Book'
 
 dotenv.config()
 
 const port = process.env.PORT
-const app : Express = express()
+const app: Express = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(port, () =>{
-    console.log(`Running at http://localhost:${port}`)
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '090712',
+  database: 'typeorm_db',
+  entities: [Book],
+  synchronize: true
+})
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database was connected successfully')
+  })
+  .catch((error) => console.log(`Error connecting to database: ${error}`))
+
+app.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`)
 })
